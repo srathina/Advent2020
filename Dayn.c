66401742b55695c68a83308b32638d392254f61c
R <SRATHINA@volvocars.com>
2020-12-04 13:31:58 +0100

    day4 commits

---
+++ Dayn.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+void dnpart1(void);
+void dnpart2(void);
+
+void main(void)
+{
+
+    dnpart1();
+    dnpart2();
+}
+
+void dnpart1(void)
+{
+
+    printf("part1\n");
+}
+void dnpart2(void)
+{
+    printf("part2\n");
+}
